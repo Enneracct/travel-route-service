7a1c184e5a928e581205821c3c6e59713693e813
Nazar Maslov <93778419+Enneracct@users.noreply.github.com>
2023-11-25 18:18:19 +0200

    Initialized spring-boot project

---
+++ src/main/java/kpi/fam/travelroute/TravelRouteApplication.java
@@ -0,0 +1,13 @@
+package kpi.fam.travelroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRouteApplication.class, args);
+	}
+
+}
